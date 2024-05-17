3755bfe81a84b99dfb4a2139b712b6c340bbfe68
26huitailang <26huitailang@gmail.com>
2024-05-17 10:04:38 +0800

    remove zsh to notebooks and chezmoi

--- zsh/install.sh
+++
@@ -1,8 +0,0 @@
-# oh-my-zsh, already install in manjaro-i3wm/install.sh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-# 添加到.zshrc
-source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-cp .zshrc ~/.zshrc
-source ~/.zshrc
